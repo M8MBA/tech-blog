be5887dea89c4858df7d24085c5de336af0508d2
M8MBA <sauce.ix@gmail.com>
2022-05-04 17:47:52 -0600

    export update

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,23 +1,24 @@
 async function loginFormHandler(event) {
   event.preventDefault();
+  const username = document.querySelector('#username').value.trim();
+  const password = document.querySelector('#password').value.trim();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
+  if (username && password) {
+    fetch('api/users/login', {
       method: 'post',
       body: JSON.stringify({
-        email,
-        password
+        username,
+        password,
       }),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { 'Content-Type': 'application/json' },
+    }).then(response => {
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
     });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
   }
 }
+
+document.querySelector('.form').addEventListener('submit', loginFormHandler);
