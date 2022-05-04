c7f34eb7bc74d08d453720da96202ab410287796
M8MBA <sauce.ix@gmail.com>
2022-05-04 15:06:14 -0600

    signup.js

---
+++ public/javascript/signup.js
@@ -0,0 +1,27 @@
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+  const username = document.querySelector('#username').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+
+  if (username && email && password) {
+    fetch('api/users/', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then(response => {
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    });
+  }
+}
+
+document.querySelector('.form').addEventListener('submit', signupFormHandler);
