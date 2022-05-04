35e4dd87ff44bb824ebbe3b5d79de169440ce8ad
M8MBA <sauce.ix@gmail.com>
2022-05-04 14:53:08 -0600

    User, Comment, Post models

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Comment };
